629123348ead15d641d77025605eae701838dfd1
Michael <hamiki4@yahoo.com>
2026-09-14 00:07:57 +0000

    fix(test): compile persisted profile projection coverage

--- tests/Weymela.Api.IntegrationTests/OnboardingHttpTests.cs
+++ tests/Weymela.Api.IntegrationTests/OnboardingHttpTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json.Nodes;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Weymela.Api;
